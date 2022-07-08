dd1eacdd0c5f32a6585c8848d409838112320e6d
Rasujon3 <rasujon3@gmail.com>
2022-07-08 19:58:35 +0600

    global css

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 
 const slug = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   const { slug } = router.query;
   return (
