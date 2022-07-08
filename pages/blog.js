4f61e9d66c00638356d550600aa8ea09f10ff7a1
Rasujon3 <rasujon3@gmail.com>
2022-07-09 00:02:45 +0600

    create Blog & Blogpost page

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,26 @@
+import Link from "next/link";
 import React from "react";
+import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
   return (
-    <div>
-      <h2>This is blog</h2>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.blogItem}>
+          <Link href={"/blogpost/learn-js"}>
+            <h3>How t learn Javascript in 2022?</h3>
+          </Link>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How t learn Javascript in 2022?</h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How t learn Javascript in 2022?</h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+      </main>
     </div>
   );
 };
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,13 +1,27 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   const { slug } = router.query;
   return (
-    <div>
-      <h2>{slug}</h2>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+          aspernatur fugit iure dolores? Optio obcaecati ratione ipsa libero
+          voluptatem eaque qui necessitatibus nemo? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Atque corporis ad ut dicta minus fugit
+          dolorem incidunt labore placeat exercitationem iusto facilis, sapiente
+          a error odio doloremque vero dignissimos illum? Vero aspernatur odit
+          temporibus eaque eius aliquid, beatae consequuntur, a, officia sit aut
+          ducimus in.
+        </p>
+      </main>
     </div>
   );
 };
